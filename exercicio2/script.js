27c72f2a3feeac77980ead31d4517b5cd77a2b6b
Joanadayse <daysejoana3@gmail.com>
2022-07-28 16:25:02 -0300

    feito

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,27 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+// // quantidade de itens
+
+let numeros = [3 ,9,5 ,1]
+let palavras = ["amor" , "carinho" ,"paz" , "sorte"]
+let diversos = [2 , "matheus" , false]
+
+console.log(numeros.length) //4
+console.log(palavras.length) //4
+console.log(diversos.length) //3
+
+ // acessando itens 
+let primeiroItem = numeros[0]
+console.log(primeiroItem) //3
+
+let segundoItem = palavras[1]
+console.log(segundoItem) // carinho
+
+let terceiroItem = diversos[2]
+console.log(terceiroItem) //false
+
+//informação booleana sobre inclusão
+console.log(numeros.includes(2)) //false
+console.log(palavras.includes("paz")) //true
+
+
+
+
